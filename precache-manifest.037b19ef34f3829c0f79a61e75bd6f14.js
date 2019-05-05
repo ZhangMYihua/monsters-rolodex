626c752245ba2c4ca619c99871153baade5bf936
Yihua Zhang <michaelzhang2@msn.com>
2019-05-05 16:41:05 +0200

    Updates

---
+++ precache-manifest.037b19ef34f3829c0f79a61e75bd6f14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51b773f5f82ab14ad3cfa9e3a4e6ecf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a357274007152f114bbc",
+    "url": "/monsters-rolodex/static/css/main.b635b742.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a357274007152f114bbc",
+    "url": "/monsters-rolodex/static/js/main.15728b3e.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
